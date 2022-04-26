381189505b4cbfc227d092d2b5df920e6833ac47
1370179282 <81346670+1370179282@users.noreply.github.com>
2022-04-27 01:35:31 +0800

    feat:aaa

--- upload/modelUpload/modelUpload.ts
+++ upload/modelUpload/modelUpload.ts
@@ -17,14 +17,7 @@ var storage = multer.diskStorage({
   destination: path.resolve(__dirname, "../../db/models"),
   //设置 上传文件保存的文件名
   filename: function (req: any, file, cb) {
-    // 获取后缀扩展
-    // let extName = file.originalname.slice(file.originalname.lastIndexOf(".")); //.jpg
-    // // 获取名称
-    // console.log(req.files, "req.files");
-    // // console.log(file, "file");
-    // let fileName = Date.now();
-    // req.myURL = fileName + extName;
-    if (req.myFilelist[0]) {
+    if (req.myFilelist) {
       req.myFilelist.push(file.originalname);
     } else {
       req.myFilelist = [file.originalname];
